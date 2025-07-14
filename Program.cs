24c0bd1277ba690cb581bb2a714a9261111408ab
Yasin Arafat <62289617+yasinarafat-talukdar@users.noreply.github.com>
2025-07-14 17:42:52 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+using Microsoft.EntityFrameworkCore;
+using PayrollToyHRD.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddHttpContextAccessor();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Report}/{action=Index}/{id?}");
+
+app.Run();
